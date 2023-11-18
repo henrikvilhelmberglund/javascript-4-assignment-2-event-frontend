fbddf7b184313d2f160bf54fd6976c97115bc161
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-18 18:01:03 +0100

    feat: add loader to /areas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import indexImage from '../../src/assets/andree-wallin-1118.webp';
 
 export default function Index() {
 	return (
-		<main className={`bg-[url(${indexImage})] bg-blend-multiply bg-cover flex min-h-screen flex-col items-center bg-black/70`}>
+		<main className={`bg-[url(${indexImage})] flex min-h-screen flex-col items-center bg-black/70 bg-cover bg-blend-multiply`}>
 			<h1 className="font-heading drop-shadow-color-black pt-8 text-7xl text-emerald-500 drop-shadow-lg">CthulhuCon</h1>
 			<h2 className="font-elite mt-6 text-3xl text-emerald-500">
 				CthulhuCon is returning to
@@ -12,7 +12,7 @@ export default function Index() {
 				Don't miss it!
 			</h2>
 			<div className="font-elite border-1 mt-20 w-[45%] rounded border-emerald-500 p-4 text-emerald-500 backdrop-blur-md">
-				<p className="text-2xl font-extrabold mb-2">Schedule</p>
+				<p className="mb-2 text-2xl font-extrabold">Schedule</p>
 				<p>09:00 CthulhuCon opens</p>
 				<p>10:00 Signing event at book area</p>
 				<p>11:00 Professional photo event starts at photo area (note: special ticket needed)</p>
@@ -22,7 +22,6 @@ export default function Index() {
 				<p>17:00 Cosplay event prize ceremony</p>
 				<p>18:00 Special event!</p>
 			</div>
-			{/* <img className="absolute left-0 w-full opacity-20" src={indexImage} alt="Cthulhu illustration by Andree Wallin" /> */}
 		</main>
 	);
 }
