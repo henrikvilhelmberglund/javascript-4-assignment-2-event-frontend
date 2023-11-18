87df7f5c64e83c440b3f3e3fbbb9e95ff63b3671
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-18 15:26:56 +0100

    fix: remove ewallet example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,24 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Layout() {
-  return (
-    <>
-      <div className="w-screen min-h-screen pb-52 bg-gradient-to-b from-white to-sky-200 dark:bg-sky-900">
-        <header>
-          <nav className="flex flex justify-center gap-12 items-center bg-slate-100 py-2">
-            <Link className="nav-button" to="/">
-              Home
-            </Link>
-            <Link className="nav-button" to="/ewallet">
-              E-wallet example
-            </Link>
-            <Link className="nav-button" to="/jotai-test">
-              Jotai example
-            </Link>
-          </nav>
-        </header>
-        <main className="flex flex-col items-center min-h-screen md:mt-20 md:mb-40">
-          <Outlet />
-        </main>
-        <footer className="fixed bottom-2 right-2">Footer</footer>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="min-h-screen w-screen bg-gradient-to-b from-white to-sky-200 pb-52 dark:bg-sky-900">
+				<header>
+					<nav className="flex flex items-center justify-center gap-12 bg-slate-100 py-2">
+						<Link className="nav-button" to="/">
+							Home
+						</Link>
+						<Link className="nav-button" to="/jotai-test">
+							Jotai example
+						</Link>
+					</nav>
+				</header>
+				<main className="flex min-h-screen flex-col items-center md:mb-40 md:mt-20">
+					<Outlet />
+				</main>
+				<footer className="fixed bottom-2 right-2">Footer</footer>
+			</div>
+		</>
+	);
 }
