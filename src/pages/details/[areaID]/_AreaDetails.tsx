1e321ef074fb4c7115ef22cd965cae1ba703a00e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-19 18:12:37 +0100

    feat: add todo

--- src/pages/details/[areaID]/_AreaDetails.tsx
+++ src/pages/details/[areaID]/_AreaDetails.tsx
@@ -6,7 +6,8 @@ export default function AreaDetails({ name, image_url: imageUrl, description }: 
 		<article
 			style={{ backgroundImage: `url(${imageUrl})` }}
 			className="outline-solid flex h-[80vh] w-full flex-col items-center bg-black/70 bg-cover bg-blend-multiply">
-			<h2 className="font-elite relative top-6 w-96 text-center text-5xl text-emerald-500">{name}</h2>
+      <h2 className="font-elite relative top-6 w-96 text-center text-5xl text-emerald-500">{name}</h2>
+      {/* TODO display always on mobile */}
 			<section style={{ backgroundImage: `url(${bookImage})` }} className="hover:opacity-100 opacity-0 transition-all duration-500 mt-12 bg-black bg-cover">
 				<div className="flex w-[70vw] h-[34vw] flex-col p-4 flex-wrap">{description && description.split('\n').map((paragraph) => <p className="font-elite first-letter:text-xl px-4 w-[35vw] text-black ">{paragraph}</p>)}</div>
 			</section>
