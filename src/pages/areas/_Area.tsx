3995ba723d9e9e5b18e782bcc1e220551010ea8e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-22 15:26:48 +0100

    fix: change a to Link, add view transition

--- src/pages/areas/_Area.tsx
+++ src/pages/areas/_Area.tsx
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
 import { IShortAreaData } from '../../lib/interfaces/IAreas';
 
 export default function Area({ id, name, image_url: imageUrl }: IShortAreaData) {
 	return (
-		<a
+		<Link
 			style={{ backgroundImage: `url(${imageUrl})` }}
 			className="outline-solid m-4 w-min bg-black/60 bg-cover px-10 bg-blend-multiply outline-0  outline-emerald-500 hover:outline-1"
-			href={`/details/${id}`}>
+			to={`/details/${id}`}
+			unstable_viewTransition>
 			<article>
-				<h2 className="font-elite relative top-6 w-96 text-center  text-5xl text-emerald-500">{name}</h2>
+				<h2 className="font-elite relative top-6 w-96 text-center text-5xl text-emerald-500">{name}</h2>
 			</article>
-		</a>
+		</Link>
 	);
 }
