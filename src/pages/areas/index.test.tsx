fa4c30b35bf4e2a0862a32ba8f4c03a5afbb134f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-23 16:52:06 +0100

    test: cleanup

--- src/pages/areas/index.test.tsx
+++ src/pages/areas/index.test.tsx
@@ -1,15 +1,9 @@
-import { render } from '@testing-library/react';
-import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import Index, { Loader } from '.';
+import { Loader } from '.';
 import { ILoaderAreasResponse, IShortAreaData } from '../../lib/interfaces/IAreas';
 
 describe('Areas loader', async () => {
 	// const router = createMemoryRouter([{ path: '/', element: <Index /> }], { initialEntries: ['/'] });
-
 	// const { container } = render(<RouterProvider router={router} />);
-
-	// expect('Photo area').toBeInTheDocument();
-	// expect(container.getByText(/Photo area/i)).toBeInTheDocument();
 
 	const data = await Loader();
 	const json: ILoaderAreasResponse = await data.json();
