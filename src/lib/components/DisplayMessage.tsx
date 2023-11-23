f2ab9e7d4678deeec86cd5ea489e4abbedd63c87
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-23 15:13:13 +0100

    refactor: cleanup

--- src/lib/components/DisplayMessage.tsx
+++ src/lib/components/DisplayMessage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 type DisplayMessageProps = {
 	text: string;
@@ -6,11 +6,9 @@ type DisplayMessageProps = {
 };
 
 export default function DisplayMessage({ text }: DisplayMessageProps) {
-	const [isVisible, setIsVisible] = useState(true);
-
 	return (
 		<>
-			{isVisible && text !== '' ?
+			{text !== '' ?
 				<div className="outline-solid absolute -left-40 top-0 w-max animate-[slide-out-down-custom_1.0s_ease-out_1] rounded bg-green-500 p-4 outline-1 outline-black">
 					<p className="text-2xl text-black">{text}</p>
 				</div>
