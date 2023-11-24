91ddcd5549439c23c06d8ecfc7771d808b7e8469
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-24 17:43:36 +0100

    feat: use HelmetProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 // import { Routes } from "generouted/react-router";
 import { Routes } from './routes';
+import { HelmetProvider } from 'react-helmet-async';
 
 import 'virtual:uno.css';
 import '@unocss/reset/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Routes />
+		<HelmetProvider>
+			<Routes />
+		</HelmetProvider>
 	</React.StrictMode>
 );
