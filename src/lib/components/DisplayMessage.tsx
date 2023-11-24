35dacd7e6bc3668785f8c8fd88151276b527f52d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-24 17:12:09 +0100

    fix: add keyframes-slide-out-down-custom because animate is using an arbitrary animation

--- src/lib/components/DisplayMessage.tsx
+++ src/lib/components/DisplayMessage.tsx
@@ -6,7 +6,7 @@ export default function DisplayMessage({ text }: DisplayMessageProps) {
 	return (
 		<>
 			{text !== '' ?
-				<div className="outline-solid absolute -left-40 top-0 w-max animate-[slide-out-down-custom_1.0s_ease-out_1] rounded bg-green-500 p-4 outline-1 outline-black">
+				<div className="outline-solid keyframes-slide-out-down-custom absolute -left-40 top-0 w-max animate-[slide-out-down-custom_1.5s_ease-out_1] rounded bg-green-500 p-4 outline-1 outline-black">
 					<p className="text-2xl text-black">{text}</p>
 				</div>
 			:	null}
