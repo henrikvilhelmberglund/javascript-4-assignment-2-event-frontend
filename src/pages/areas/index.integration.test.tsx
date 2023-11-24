bbde29156bb7884e2e1819222d2cb55c27b99cc0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-24 21:45:24 +0100

    fix: make areas loader test more typescripty

--- src/pages/areas/index.integration.test.tsx
+++ src/pages/areas/index.integration.test.tsx
@@ -1,12 +1,14 @@
 import { Loader } from '.';
 import { ILoaderAreasResponse, IShortAreaData } from '../../lib/interfaces/IAreas';
+import type { LoaderFunctionArgs } from 'react-router';
 
 describe('Areas loader', async () => {
 	// const router = createMemoryRouter([{ path: '/', element: <Index /> }], { initialEntries: ['/'] });
 	// const { container } = render(<RouterProvider router={router} />);
 
-	const data = await Loader();
-	const json: ILoaderAreasResponse = await data.json();
+  const data = await Loader({} as LoaderFunctionArgs<Response>);
+  const response = data as Response;
+	const json: ILoaderAreasResponse = await response.json();
 	const areas: IShortAreaData[] = json.data;
 
 	it.each(areas)(`$name has id, name and image_url`, async (area: IShortAreaData) => {
